44d292d12e21a0dcedd4eaa2047b1e292e4588f6
Edwin Garcia <egarciavalle2014@gmail.com>
2023-11-30 00:57:13 -0600

    chore: create mock data for offers
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- cmd/core/handlers.go
+++ cmd/core/handlers.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"strconv"
+
+	"edwingarcia.dev/github/jhunterscore/pkg/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 func (core *Core) HandldeOffers(c *fiber.Ctx) error {
 	isCompact := c.QueryBool("c", false)
-	_ = isCompact
+
+	if isCompact {
+		c.JSON(fiber.Map{
+			"ok":   true,
+			"data": getCompactOffers(),
+		})
+		return nil
+	}
+
+	c.JSON(fiber.Map{
+		"ok":   true,
+		"data": mockOffers,
+	})
+
 	return nil
 }
 
 func (core *Core) HandldeOfferById(c *fiber.Ctx) error {
+	// Get offer id from params.
+	id := c.Params("id")
+
+	// Find offer by id.
+	var offer models.Offer
+
+	for _, o := range mockOffers {
+		if strconv.Itoa(o.Id) == id {
+			offer = o
+			break
+		}
+	}
+
+	if offer.Id == 0 {
+		c.JSON(fiber.Map{
+			"ok":    false,
+			"error": "Oferta no encontrada",
+		})
+		return nil
+	}
+
+	c.JSON(fiber.Map{
+		"ok":   true,
+		"data": offer,
+	})
+
 	return nil
 }
 
