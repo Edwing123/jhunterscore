0921e9948f6d4b5bc301944c405ce2c00899a71c
Edwin Garcia <egarciavalle2014@gmail.com>
2023-11-30 00:10:33 -0600

    chore: define `Salary` field as float64
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,29 +1,29 @@
 package models
 
 type Offer struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Role        string `json:"role"`
-	Company     string `json:"company"`
-	Content     string `json:"content"`
-	Contract    string `json:"contract"`
-	Location    string `json:"location"`
-	Salary      string `json:"salary"`
-	ContactInfo string `json:"contact_info"`
-	CreatedAt   string `json:"created_at"`
-	Author      string `json:"author,omitempty"`
-	IsPublished bool   `json:"is_published,omitempty"`
+	Id          int     `json:"id"`
+	Title       string  `json:"title"`
+	Role        string  `json:"role"`
+	Company     string  `json:"company"`
+	Content     string  `json:"content"`
+	Contract    string  `json:"contract"`
+	Location    string  `json:"location"`
+	Salary      float64 `json:"salary"`
+	ContactInfo string  `json:"contact_info"`
+	CreatedAt   string  `json:"created_at"`
+	Author      string  `json:"author,omitempty"`
+	IsPublished bool    `json:"is_published,omitempty"`
 }
 
 type CompactOffer struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Role        string `json:"role"`
-	Company     string `json:"company"`
-	Contract    string `json:"contract"`
-	Salary      string `json:"salary"`
-	CreatedAt   string `json:"created_at"`
-	IsPublished bool   `json:"is_published,omitempty"`
+	Id          int     `json:"id"`
+	Title       string  `json:"title"`
+	Role        string  `json:"role"`
+	Company     string  `json:"company"`
+	Contract    string  `json:"contract"`
+	Salary      float64 `json:"salary"`
+	CreatedAt   string  `json:"created_at"`
+	IsPublished bool    `json:"is_published,omitempty"`
 }
 
 type Resource struct {
