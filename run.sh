36f1162859fc5ba3e0a83ae49e6d5f7579f3a405
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-01 21:40:30 -0600

    chore: fix typo for config name in run script
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 BUILD_TAGS="sqlite_fts5 sqlite_foreign_keys"
 
-go run -tags "$BUILD_TAGS" ./cmd/core -config=./config.exampe.json
+go run -tags "$BUILD_TAGS" ./cmd/core -config=./config.example.json
