b837f08d3cc206eaf85a251029e09d437c6a598c
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-02 01:35:55 -0600

    feat: implement Users repo in SQLite
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- pkg/database/sqlite/init.go
+++ pkg/database/sqlite/init.go
@@ -1,6 +1,20 @@
 package sqlite
 
-import _ "embed"
+import (
+	_ "embed"
+	"errors"
+
+	"edwingarcia.dev/github/jhunterscore/pkg/database"
+	"github.com/mattn/go-sqlite3"
+)
 
 //go:embed base.db
 var INITIALIZED_DATABASE_DATA []byte
+
+func isUniqueViolationErr(err sqlite3.Error) bool {
+	return err.ExtendedCode == sqlite3.ErrConstraintUnique
+}
+
+func internalDatabaseErr(err error) error {
+	return errors.Join(database.ErrInternalDatabase, err)
+}
---
+++ pkg/database/sqlite/user.go
@@ -0,0 +1,125 @@
+package sqlite
+
+import (
+	"database/sql"
+	"errors"
+	"strings"
+
+	"edwingarcia.dev/github/jhunterscore/pkg/database"
+	"edwingarcia.dev/github/jhunterscore/pkg/database/models"
+	"github.com/mattn/go-sqlite3"
+)
+
+type User struct {
+	db *sql.DB
+}
+
+func (u *User) isUsernameTakenErr(msg string) bool {
+	return strings.Contains(msg, "username")
+}
+
+func (u *User) isFirstNameTakenErr(msg string) bool {
+	return strings.Contains(msg, "first_name")
+}
+
+func (u *User) getUserErr(msg string) (err error) {
+	switch true {
+	case u.isUsernameTakenErr(msg):
+		err = database.ErrUsernameTaken
+	case u.isFirstNameTakenErr(msg):
+		err = database.ErrFirstNameTaken
+	}
+
+	return
+}
+
+func (u *User) GetById(id int) (models.User, error) {
+	var user models.User
+
+	row := u.db.QueryRow(SELECT_USER_BY_ID, id)
+
+	err := row.Scan(
+		&user.Id,
+		&user.Username,
+		&user.FirstName,
+		&user.LastName,
+		&user.Role,
+		&user.CreatedAt,
+		&user.IsActive,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return user, database.ErrNoRows
+		}
+
+		return user, internalDatabaseErr(err)
+	}
+
+	return user, nil
+}
+
+func (u *User) GetAll() ([]models.User, error) {
+	var users []models.User
+
+	rows, err := u.db.Query(SELECT_ALL_USERS)
+	if err != nil {
+		return nil, internalDatabaseErr(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var user models.User
+
+		err := rows.Scan(
+			&user.Id,
+			&user.Username,
+			&user.FirstName,
+			&user.LastName,
+			&user.Role,
+			&user.CreatedAt,
+			&user.IsActive,
+		)
+		if err != nil {
+			return nil, internalDatabaseErr(err)
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (u *User) Create(user models.User) (models.User, error) {
+	result, err := u.db.Exec(
+		INSERT_USER,
+		user.Username,
+		user.Password,
+		user.FirstName,
+		user.LastName,
+		user.RoleId,
+	)
+	if err != nil {
+		var sqliteErr sqlite3.Error
+
+		if errors.As(sqliteErr, &sqliteErr) && isUniqueViolationErr(sqliteErr) {
+			msg := sqliteErr.Error()
+			err = u.getUserErr(msg)
+			return models.User{}, err
+		}
+
+		return models.User{}, internalDatabaseErr(err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return models.User{}, internalDatabaseErr(err)
+	}
+
+	user.Id = int(id)
+	return user, nil
+}
+
+func (u *User) Update(user models.User) (models.User, error) {
+	return user, nil
+}
