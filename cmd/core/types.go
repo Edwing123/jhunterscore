a274c5f5b97d9f05fa3ef5d81a5d48c566cfd807
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-09 15:47:14 -0600

    chore: add `Database` field to `Core` struct
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- cmd/core/types.go
+++ cmd/core/types.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log/slog"
 
+	"edwingarcia.dev/github/jhunterscore/pkg/database/models"
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
 
 type Core struct {
-	Logger *slog.Logger
-	Store  *session.Store
+	Logger   *slog.Logger
+	Store    *session.Store
+	Database models.Database
 }
 
 type Config struct {
