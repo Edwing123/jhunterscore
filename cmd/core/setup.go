6cad4c48d11468da72814af516e3de3eed167f4a
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-09 15:12:55 -0600

    chore: define base views endpoints
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- cmd/core/setup.go
+++ cmd/core/setup.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 
-	"edwingarcia.dev/github/jhunterscore/pkg/database/models"
 	"edwingarcia.dev/github/jhunterscore/ui"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
@@ -62,23 +61,47 @@ func (core *Core) Setup() *fiber.App {
 func (core *Core) SetupAdmin(app *fiber.App) {
 	admin := app.Group("/admin")
 
-	// Protect UI pages by setting several HTTP headers.
-	admin.Use(helmet.New())
-
-	user := models.User{
-		Role:     "admin",
-		Username: "Edwing123",
-	}
-
-	admin.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("pages/admin/home/index", ViewData{
-			Path:  c.Path(),
-			User:  user,
-			Links: links,
-		})
-	})
+	admin.Use(
+		helmet.New(),
+	)
 
 	admin.Get("/login", func(c *fiber.Ctx) error {
 		return c.Render("pages/admin/auth/login", nil)
+	})
+
+	admin.Use(
+		core.RequireAuth,
+	)
+
+	admin.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/admin/home/index", nil)
+	})
+
+	// Offers pages.
+	offers := admin.Group("/offers")
+
+	offers.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/admin/offers/index", nil)
+	})
+
+	// Resources pages.
+	resources := admin.Group("/resources")
+
+	resources.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/admin/resources/index", nil)
+	})
+
+	// Files pages.
+	files := admin.Group("/files")
+
+	files.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/admin/files/index", nil)
+	})
+
+	// Companies pages.
+	companies := admin.Group("/companies", core.RequireAdmin)
+
+	companies.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/admin/companies/index", nil)
 	})
 }
