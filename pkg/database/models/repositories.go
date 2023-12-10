a763ec587b59016103fa4d38b5f26a1ff31876d9
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-10 08:24:17 -0600

    chore: create repo interfaces for offers and resources
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- pkg/database/models/repositories.go
+++ pkg/database/models/repositories.go
@@ -5,6 +5,8 @@ type Database struct {
 	UsersRepository     UsersRepository
 	FilesRepository     FilesRepository
 	CompaniesRepository CompaniesRepository
+	OffersRepository    OffersRepository
+	ResourcesRepository ResourcesRepository
 }
 
 type AuthRepository interface {
@@ -32,5 +34,23 @@ type CompaniesRepository interface {
 	GetAll() ([]Company, error)
 	Create(company Company) (Company, error)
 	Update(company Company) (Company, error)
+	Delete(id int) error
+}
+
+type OffersRepository interface {
+	GetById(id int) (Offer, error)
+	GetUserIdByOfferId(id int) (int, error)
+	GetAll() ([]Offer, error)
+	Create(offer Offer) (Offer, error)
+	Update(offer Offer) (Offer, error)
+	Delete(id int) error
+}
+
+type ResourcesRepository interface {
+	GetById(id int) (Resource, error)
+	GetUserIdByResourceId(id int) (int, error)
+	GetAll() ([]Resource, error)
+	Create(resource Resource) (Resource, error)
+	Update(resource Resource) (Resource, error)
 	Delete(id int) error
 }
