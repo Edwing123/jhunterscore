c1d1e4a2fc748855e73a89497626babe3d85a2b4
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-10 04:06:27 -0600

    chore(sqlite): fix column ambiguity in query
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- pkg/database/sqlite/queries.go
+++ pkg/database/sqlite/queries.go
@@ -36,7 +36,7 @@ const (
 
 	// File queries.
 	SELECT_FILE_BY_ID = `
-	SELECT "file_id", "name", "mime_type", "path", "size", ("u"."first_name" || ' ' || "u"."last_name") AS "author", "created_at"
+	SELECT "file_id", "name", "mime_type", "path", "size", ("u"."first_name" || ' ' || "u"."last_name") AS "author", "f"."created_at"
 	FROM "files" AS "f"
 	INNER JOIN "users" AS "u"
 	ON "f"."user_id" = "u"."user_id"
