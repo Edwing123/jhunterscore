75e1b0821e0c0842085156190951d81a62296719
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-10 05:52:45 -0600

    chore: add return statement in handler responses
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- cmd/core/handlers.go
+++ cmd/core/handlers.go
@@ -11,19 +11,16 @@ func (core *Core) HandldeOffers(c *fiber.Ctx) error {
 	isCompact := c.QueryBool("c", false)
 
 	if isCompact {
-		c.JSON(fiber.Map{
+		return c.JSON(fiber.Map{
 			"ok":   true,
 			"data": getCompactOffers(mockOffers),
 		})
-		return nil
 	}
 
-	c.JSON(fiber.Map{
+	return c.JSON(fiber.Map{
 		"ok":   true,
 		"data": mockOffers,
 	})
-
-	return nil
 }
 
 func (core *Core) HandldeOfferById(c *fiber.Ctx) error {
@@ -41,38 +38,32 @@ func (core *Core) HandldeOfferById(c *fiber.Ctx) error {
 	}
 
 	if offer.Id == 0 {
-		c.JSON(fiber.Map{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"ok":    false,
 			"error": "Oferta no encontrada",
 		})
-		return nil
 	}
 
-	c.JSON(fiber.Map{
+	return c.JSON(fiber.Map{
 		"ok":   true,
 		"data": offer,
 	})
-
-	return nil
 }
 
 func (core *Core) HandldeResources(c *fiber.Ctx) error {
 	isCompact := c.QueryBool("c", false)
 
 	if isCompact {
-		c.JSON(fiber.Map{
+		return c.JSON(fiber.Map{
 			"ok":   true,
 			"data": getCompactResources(mockResources),
 		})
-		return nil
 	}
 
-	c.JSON(fiber.Map{
+	return c.JSON(fiber.Map{
 		"ok":   true,
 		"data": mockResources,
 	})
-
-	return nil
 }
 
 func (core *Core) HandldeResourceById(c *fiber.Ctx) error {
@@ -90,19 +81,16 @@ func (core *Core) HandldeResourceById(c *fiber.Ctx) error {
 	}
 
 	if resource.Id == 0 {
-		c.JSON(fiber.Map{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"ok":    false,
 			"error": "Recurso no encontrado",
 		})
-		return nil
 	}
 
-	c.JSON(fiber.Map{
+	return c.JSON(fiber.Map{
 		"ok":   true,
 		"data": resource,
 	})
-
-	return nil
 }
 
 func (core *Core) HandldeCompanies(c *fiber.Ctx) error {
