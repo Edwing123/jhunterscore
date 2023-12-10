8f3913b7b345e116cf6c68b82c91a5b3f0bfd039
Edwin Garcia <egarciavalle2014@gmail.com>
2023-12-10 02:29:14 -0600

    chore: add more constants
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- cmd/core/constants.go
+++ cmd/core/constants.go
@@ -5,10 +5,15 @@ const (
 	USER_ROLE_KEY    = "user_role"
 	USER_ID_KEY      = "user_id"
 	IS_LOGGED_IN_KEY = "is_logged"
+	ERRORS_KEY       = "errors"
 
 	DATA_DIR_LOGS_DIR    = "logs"
 	DATA_DIR_SESSION_DIR = "sessions"
+	DATA_DIR_FILES_DIR   = "files"
 	LOGS_FILE            = "logs.txt"
 	SESSION_FILE         = "sessions.db"
 	DATABASE_FILE        = "jobshunters.db"
+
+	// The file can be of 5MB maximum
+	MAX_FILE_SIZE = 1024 * 1024 * 5
 )
